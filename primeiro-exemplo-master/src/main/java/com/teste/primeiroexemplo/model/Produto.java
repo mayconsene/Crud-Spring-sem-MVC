67a25aaba39c14af3691b62a1d34e2bba1969791
github.com/mayconsene <mayconmoraessene@gmail.com>
2022-06-06 21:19:23 -0300

    Crud Spring sem MVC 06-7-2022

---
+++ primeiro-exemplo-master/src/main/java/com/teste/primeiroexemplo/model/Produto.java
@@ -0,0 +1,69 @@
+package com.teste.primeiroexemplo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Produto {
+
+    // #region Atributos
+    @Id // vai transformar essa coluna em primary key
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String nome;
+
+    private Integer quantidade;
+
+    private Double valor;
+
+    private String observacao;
+
+    // #endregion
+
+    // #region Getters e Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    // #endregion
+
+}
---
+++ primeiro-exemplo-master/src/main/java/com/teste/primeiroexemplo/services/ProdutoService.java
@@ -0,0 +1,76 @@
+package com.teste.primeiroexemplo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.teste.primeiroexemplo.model.Produto;
+import com.teste.primeiroexemplo.repository.ProdutoRepository;
+//import com.teste.primeiroexemplo.repository.ProdutoRepository_old;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    /**
+     * Método para retornar uma lista de produtos
+     * 
+     * @return Lista de produtos.
+     */
+    public List<Produto> obterTodos() {
+        return produtoRepository.findAll();
+    }
+
+    /**
+     * Método que retorna o produto encontrado pelo seu Id.
+     * 
+     * @param id do produto que será localizado
+     * @return Retorna um produto caso tenha encontrado.
+     */
+
+    public Optional<Produto> obterPorId(Integer id) {
+        return produtoRepository.findById(id);
+    }
+
+    /**
+     * Método para adicionar produto na lista
+     * 
+     * @param produto que será adicionado
+     * @return produto que adicionado na lista.
+     */
+    public Produto adicionar(Produto produto) {
+        // Poderia ter alguma regra de negócio para validar produto.
+        return produtoRepository.save(produto);
+    }
+
+    /**
+     * Método para deletar o produto por id
+     * 
+     * @param id do produto a ser deletado
+     */
+    public void deletar(Integer id) {
+        // Poderia ter alguma lógica de validação aqui.
+        produtoRepository.deleteById(id);
+
+    }
+
+    /**
+     * Método para atualizar o produto na lista
+     * 
+     * @param produto que será atualizado
+     * @param id do produto.
+     * @return Retorna o produto após atualizar a lista.
+     */
+    public Produto atualizar(Integer id, Produto produto) {
+
+        // Poderia ter alguma validação no ID.
+        produto.setId(id);
+
+        return produtoRepository.save(produto);
+    }
+}
